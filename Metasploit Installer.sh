1c0f2c19a5af3a773b26c9d833cf9a2bbb2c6fc0
HEXER22 <40181915+HEXER22@users.noreply.github.com>
2018-07-29 00:42:39 +0530

    Update Metasploit Installer.sh

--- Metasploit Installer.sh
+++ Metasploit Installer.sh
@@ -2,15 +2,26 @@
 
 echo "Doing a update for the latest updates" 
 
+sleep 3
+
 apt-get update && apt-get upgrade -y
 
 echo "Installing Metasploit Framework on your Linux Terminal"
+sleep 3
 
 pkg install wget
+
 echo "Going to download all required files for Metasploit Framework"
+
+sleep 3
+
 wget https://Auxilus.github.io/metasploit.sh
+
 echo " Going to run Metasploit Installer "
-bash metasploit.sh && echo "Metasploit is successfully installed on  your Linux Terminal , Thank You for Using HEXER22 Program"
+
+sleep 3
+
+chmod +x metasploit.sh && echo "Metasploit is successfully installed on  your Linux Terminal , Thank You for Using HEXER22 Program"
 
 
 
